95cfbbb85f0058f4fbb8802645487b0502582c45
Nadia H <144337411+Shiihon@users.noreply.github.com>
2024-05-06 00:25:46 +0200

    empty controller class implemented

---
+++ src/main/java/app/controllers/OrderController.java
@@ -0,0 +1,36 @@
+package app.controllers;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class OrderController {
+
+    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
+
+    }
+
+    public void sendOrderRequest(Context ctx, ConnectionPool connectionPool) {
+
+    }
+
+    public void viewMyOrders(Context ctx, ConnectionPool connectionPool) {
+
+    }
+
+    public void payOrder(Context ctx, ConnectionPool connectionPool) {
+
+    }
+
+    public void viewInvoice(Context ctx, ConnectionPool connectionPool) {
+
+    }
+
+    public void cancelOrder(Context ctx, ConnectionPool connectionPool) {
+
+    }
+
+    public void viewCarportSchematic(Context ctx) {
+
+    }
+}
