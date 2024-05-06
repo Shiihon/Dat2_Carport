b2a168f737bd66ade41c78ad951af6df52ac4199
Nadia H <144337411+Shiihon@users.noreply.github.com>
2024-05-06 13:40:08 +0200

    WareHouseController added

---
+++ src/main/java/app/controllers/WareHouseController.java
@@ -0,0 +1,16 @@
+package app.controllers;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class WareHouseController {
+
+    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
+
+    }
+
+    public void addNewMaterial (Context ctx, ConnectionPool connectionPool){
+
+    }
+}
