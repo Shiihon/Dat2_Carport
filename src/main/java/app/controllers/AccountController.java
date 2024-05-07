d32208365895eee536c5fddbe019b674c2490381
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-05-07 12:24:48 +0200

    Implement login

--- src/main/java/app/controllers/AccountController.java
+++ src/main/java/app/controllers/AccountController.java
@@ -1,5 +1,8 @@
 package app.controllers;
 
+import app.entities.Account;
+import app.exceptions.DatabaseException;
+import app.persistence.AccountMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
@@ -7,16 +10,28 @@ import io.javalin.http.Context;
 
 public class AccountController {
 
-    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
-
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/login", ctx -> ctx.render("login.html"));
+        app.post("/login", ctx -> login(ctx, connectionPool));
     }
 
     public void createAccount(Context ctx, ConnectionPool connectionPool) {
 
     }
 
-    public void login(Context ctx, ConnectionPool connectionPool) {
+    public static void login(Context ctx, ConnectionPool connectionPool) {
+        String email = ctx.formParam("email");
+        String password = ctx.formParam("password");
 
+        try {
+            Account account = AccountMapper.login(email, password, connectionPool);
+
+            ctx.sessionAttribute("currentAccount", account);
+            ctx.redirect("/");
+        } catch (DatabaseException e) {
+            ctx.attribute("error", e.getMessage());
+            ctx.render("login.html");
+        }
     }
 
     public void logout(Context ctx, ConnectionPool connectionPool) {
