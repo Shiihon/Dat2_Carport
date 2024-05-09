1cfee166cce67dab2270f7a896c55710787cf247
Patrick Nyboe Kjøller <Patrick@ctrl.dk>
2024-05-09 23:31:52 +0200

    Schematic update

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -10,12 +10,15 @@ import io.javalin.rendering.template.JavalinThymeleaf;
 
 public class Main {
 
+    /*
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
     private static final String DB = "cupcake_development";
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+     */
 
     public static void main(String[] args) {
         // Initializing Javalin and Jetty webserver
@@ -28,7 +31,7 @@ public class Main {
 
         // Routing
 
-        AccountController.addRoutes(app, connectionPool);
+        // AccountController.addRoutes(app, connectionPool);
         app.get("/", ctx -> ctx.render("Frontpage.html"));
         app.get("/carportSchematic", ctx -> OrderController.viewCarportSchematic(ctx));
     }
