3255927e2bf3a811b628a4c4b30037fad7c8caca
Nadia H <144337411+Shiihon@users.noreply.github.com>
2024-05-13 14:09:06 +0200

    viewOrderDetails method implemented

--- src/main/java/app/controllers/SalesController.java
+++ src/main/java/app/controllers/SalesController.java
@@ -1,13 +1,19 @@
 package app.controllers;
 
+import app.entities.Order;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.OrderMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.util.Objects;
+
 
 public class SalesController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-
+        app.get("/viewOrderDetails", ctx -> viewOrderDetails(ctx, connectionPool));
     }
 
     public static void approveReview(Context ctx, ConnectionPool connectionPool) {
@@ -15,7 +21,7 @@ public class SalesController {
     }
 
     public static void updatePrice(Context ctx, ConnectionPool connectionPool) {
-
+        // lav den!
     }
 
     public static void viewCostumersOrders(Context ctx, ConnectionPool connectionPool) {
@@ -26,7 +32,16 @@ public class SalesController {
 
     }
 
-    public static void viewOrderDetails(Context ctx, ConnectionPool connectionPool) {
+    public static void viewOrderDetails(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+        int orderId = Integer.parseInt(Objects.requireNonNull(ctx.queryParam("orderId")));
 
+        try {
+            Order order = OrderMapper.getOrderById(orderId, connectionPool);
+            ctx.attribute("order", order);
+            ctx.render("orderdetails.html");
+
+        } catch (DatabaseException e) {
+            throw new DatabaseException("Failed to recieve order details");
+        }
     }
 }
