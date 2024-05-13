eec18dc2ccbb02d2b622250e5f6d20e470eedd3b
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-05-13 11:44:33 +0200

    Change to use index.html

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -29,7 +29,7 @@ public class Main {
 
         // Routing
 
-        app.get("/", ctx -> ctx.render("Frontpage.html"));
+        app.get("/", ctx -> ctx.render("index.html"));
         AccountController.addRoutes(app, connectionPool);
         OrderController.addRoutes(app, connectionPool);
         WarehouseController.addRoutes(app, connectionPool);
