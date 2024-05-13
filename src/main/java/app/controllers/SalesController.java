9092f4aa93b7c80b752aedc62ceb6139238942b2
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-05-13 14:30:41 +0200

    Implement part of viewRequests

--- src/main/java/app/controllers/SalesController.java
+++ src/main/java/app/controllers/SalesController.java
@@ -1,13 +1,29 @@
 package app.controllers;
 
+import app.entities.Order;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.OrderMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.List;
+
 public class SalesController {
 
-    public void addRoutes(Javalin app, ConnectionPool connectionPool) {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/requests", ctx -> viewRequests(ctx, connectionPool));
+    }
 
+    public static void viewRequests(Context ctx, ConnectionPool connectionPool) {
+        try {
+            List<Order> requests = OrderMapper.getAllOrdersByStatus(Order.OrderStatus.WAITING_FOR_REVIEW, connectionPool);
+
+            ctx.attribute("requests", requests);
+            ctx.render("customer-requests.html");
+        } catch (DatabaseException e) {
+            ctx.attribute("error", e.getMessage());
+        }
     }
 
     public void approveReview(Context ctx, ConnectionPool connectionPool) {
