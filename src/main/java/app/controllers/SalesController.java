a0d375a8f65137dddab16649c05bd1ea2c42fa12
Nadia H <144337411+Shiihon@users.noreply.github.com>
2024-05-14 13:56:47 +0200

    viewOrderDetails, updatePrice + routing added

--- src/main/java/app/controllers/SalesController.java
+++ src/main/java/app/controllers/SalesController.java
@@ -1,7 +1,9 @@
 package app.controllers;
 
+import app.entities.Account;
 import app.entities.Order;
 import app.exceptions.DatabaseException;
+import app.persistence.AccountMapper;
 import app.persistence.ConnectionPool;
 import app.persistence.OrderMapper;
 import io.javalin.Javalin;
@@ -14,6 +16,7 @@ public class SalesController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/viewOrderDetails", ctx -> viewOrderDetails(ctx, connectionPool));
+        app.post("updatePrice", ctx -> updatePrice(ctx, connectionPool));
     }
 
     public static void approveReview(Context ctx, ConnectionPool connectionPool) {
@@ -21,7 +24,18 @@ public class SalesController {
     }
 
     public static void updatePrice(Context ctx, ConnectionPool connectionPool) {
-        // lav den!
+        int orderId = Integer.parseInt(Objects.requireNonNull(ctx.formParam("orderId")));
+        double newPrice = Double.parseDouble(ctx.formParam("price"));
+
+        try {
+            OrderMapper.setOrderPrice(orderId, newPrice, connectionPool);
+            ctx.sessionAttribute("message", "Prisen er nu opdateret");
+            ctx.redirect(String.format("/viewOrderDetails?orderId=%d", orderId));
+
+        } catch (DatabaseException e) {
+            ctx.attribute("error", e.getMessage());
+            ctx.render("viewOrderDetails.html");
+        }
     }
 
     public static void viewCostumersOrders(Context ctx, ConnectionPool connectionPool) {
@@ -32,16 +46,19 @@ public class SalesController {
 
     }
 
-    public static void viewOrderDetails(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+    public static void viewOrderDetails(Context ctx, ConnectionPool connectionPool) {
         int orderId = Integer.parseInt(Objects.requireNonNull(ctx.queryParam("orderId")));
 
         try {
             Order order = OrderMapper.getOrderById(orderId, connectionPool);
             ctx.attribute("order", order);
-            ctx.render("orderdetails.html");
+            Account customer = AccountMapper.getAccountById(order.getAccountId(), connectionPool);
+            ctx.attribute("customer", customer);
+            ctx.render("viewOrderDetails.html");
+            ctx.sessionAttribute("message", null);
 
         } catch (DatabaseException e) {
-            throw new DatabaseException("Failed to recieve order details");
+            ctx.attribute("error", e.getMessage());
         }
     }
 }
