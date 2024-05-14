b8cde22709a4e329064cb92e54684ac7b97c5325
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-05-14 15:43:03 +0200

    Remove empty line

--- src/main/java/app/controllers/SalesController.java
+++ src/main/java/app/controllers/SalesController.java
@@ -12,7 +12,6 @@ import io.javalin.http.Context;
 import java.util.List;
 import java.util.Objects;
 
-
 public class SalesController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
