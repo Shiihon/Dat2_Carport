78fe1ceea2afaf159a2679506ada5d319959f117
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-05-16 08:23:44 +0200

    Add null check for create account

--- src/main/java/app/controllers/AccountController.java
+++ src/main/java/app/controllers/AccountController.java
@@ -9,6 +9,8 @@ import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.Objects;
+
 public class AccountController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
@@ -20,49 +22,53 @@ public class AccountController {
     }
 
     public static void createAccount(Context ctx, ConnectionPool connectionPool) {
-        String[] name = ctx.formParam("name").split(" ");
-        String address = ctx.formParam("address");
-        int zip = Integer.parseInt(ctx.formParam("postal_code"));
-        String phoneNumber = ctx.formParam("phone_number");
-        String email = ctx.formParam("email");
-        String password1 = ctx.formParam("password1");
-        String password2 = ctx.formParam("password2");
-
-        if (!password1.equals(password2)) {
-            ctx.attribute("createAccountName", String.join(" ", name));
-            ctx.attribute("createAccountAddress", address);
-            ctx.attribute("createAccountZip", zip);
-            ctx.attribute("createAccountPhoneNumber", phoneNumber);
-            ctx.attribute("createAccountEmail", email);
-
-            ctx.attribute("error", "Passwords don't match");
-            ctx.render("create-account.html");
-            return;
-        }
-
         try {
-            Account account = new Customer(email, password1, "CUSTOMER", name[0], name[1], address, zip, "City", phoneNumber);
+            String[] name = Objects.requireNonNull(ctx.formParam("name")).split(" ");
+            String address = Objects.requireNonNull(ctx.formParam("address"));
+            int zip = Integer.parseInt(Objects.requireNonNull(ctx.formParam("postal_code")));
+            String phoneNumber = Objects.requireNonNull(ctx.formParam("phone_number"));
+            String email = Objects.requireNonNull(ctx.formParam("email"));
+            String password1 = Objects.requireNonNull(ctx.formParam("password1"));
+            String password2 = Objects.requireNonNull(ctx.formParam("password2"));
 
-            AccountMapper.createAccount(account, connectionPool);
+            if (!password1.equals(password2)) {
+                ctx.attribute("createAccountName", String.join(" ", name));
+                ctx.attribute("createAccountAddress", address);
+                ctx.attribute("createAccountZip", zip);
+                ctx.attribute("createAccountPhoneNumber", phoneNumber);
+                ctx.attribute("createAccountEmail", email);
 
-            ctx.sessionAttribute("currentAccount", account);
-            String loginRedirect = ctx.sessionAttribute("loginRedirect");
-
-            if (loginRedirect != null) {
-                ctx.sessionAttribute("loginRedirect", null);
-                ctx.redirect(loginRedirect);
-            } else {
-                ctx.redirect("/");
+                ctx.attribute("error", "Passwords don't match");
+                ctx.render("create-account.html");
+                return;
             }
-        } catch (DatabaseException e) {
-            ctx.attribute("createAccountName", String.join(" ", name));
-            ctx.attribute("createAccountAddress", address);
-            ctx.attribute("createAccountZip", zip);
-            ctx.attribute("createAccountPhoneNumber", phoneNumber);
-            ctx.attribute("createAccountEmail", email);
 
-            ctx.attribute("error", e.getMessage());
-            ctx.render("create-account.html");
+            try {
+                Account account = new Customer(email, password1, "CUSTOMER", name[0], name[1], address, zip, "City", phoneNumber);
+
+                AccountMapper.createAccount(account, connectionPool);
+
+                ctx.sessionAttribute("currentAccount", account);
+                String loginRedirect = ctx.sessionAttribute("loginRedirect");
+
+                if (loginRedirect != null) {
+                    ctx.sessionAttribute("loginRedirect", null);
+                    ctx.redirect(loginRedirect);
+                } else {
+                    ctx.redirect("/");
+                }
+            } catch (DatabaseException e) {
+                ctx.attribute("createAccountName", String.join(" ", name));
+                ctx.attribute("createAccountAddress", address);
+                ctx.attribute("createAccountZip", zip);
+                ctx.attribute("createAccountPhoneNumber", phoneNumber);
+                ctx.attribute("createAccountEmail", email);
+
+                ctx.attribute("error", e.getMessage());
+                ctx.render("create-account.html");
+            }
+        } catch (NullPointerException ignored) {
+            ctx.redirect("/create-account");
         }
     }
 
