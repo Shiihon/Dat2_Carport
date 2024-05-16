1a6815c3d044b2cf2694ade9888aab6ef2a1b2db
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-05-16 15:29:03 +0200

    Create postal code mapper

---
+++ src/main/java/app/persistence/PostalCodeMapper.java
@@ -0,0 +1,35 @@
+package app.persistence;
+
+import app.entities.PostalCode;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PostalCodeMapper {
+
+    public static PostalCode getPostalCodeByZip(int zip, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "SELECT zip, city FROM postal_codes WHERE zip = ?";
+
+        try (Connection connection = connectionPool.getConnection()) {
+            try (PreparedStatement ps = connection.prepareStatement(sql)) {
+                ps.setInt(1, zip);
+
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    String city = rs.getString("city");
+
+                    return new PostalCode(zip, city);
+                } else {
+                    throw new DatabaseException("Failed to get postal code with zip = " + zip);
+                }
+            } catch (SQLException e) {
+                throw new DatabaseException("SQL Error", e.getMessage());
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Failed to connect to the database.");
+        }
+    }
+}
