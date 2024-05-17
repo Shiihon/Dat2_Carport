ef5d091f59247e3052ccfee43f3db524bc0cfeb2
Patrick Nyboe Kjøller <Patrick@ctrl.dk>
2024-05-17 12:08:40 +0200

    Added faktura option, + design

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,16 +1,15 @@
 package app.controllers;
 
-import app.entities.Customer;
-import app.entities.Order;
-import app.entities.OrderBillItem;
+import app.entities.*;
 import app.exceptions.DatabaseException;
+import app.persistence.AccountMapper;
 import app.persistence.ConnectionPool;
 import app.persistence.OrderMapper;
 import app.services.CarportSvg;
 import app.services.OrderBillGenerator;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
-
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Locale;
@@ -27,6 +26,7 @@ public class OrderController {
         app.get("backtofrontpage", ctx -> ctx.render("index.html"));
         app.get("/myorders", ctx -> viewMyOrders(ctx, connectionPool));
         app.post("/payOrder", ctx -> payOrder(ctx, connectionPool));
+        app.get("/viewInvoice", ctx -> viewInvoice(ctx, connectionPool));
     }
 
     private static void continueRequest(Context ctx, ConnectionPool connectionPool) {
@@ -109,9 +109,13 @@ public class OrderController {
 
     public static void payOrder(Context ctx, ConnectionPool connectionPool) {
         int orderId = Integer.parseInt(Objects.requireNonNull(ctx.formParam("orderId")));
+        Customer customer = ctx.sessionAttribute("currentAccount");
 
         try {
             OrderMapper.setOrderStatus(orderId, Order.OrderStatus.PAID, connectionPool);
+
+            Invoice newInvoice = new Invoice(orderId, customer.getId(), LocalDate.now());
+            OrderMapper.createOrderInvoice(newInvoice, connectionPool);
 
             ctx.redirect("myorders");
         } catch (DatabaseException e) {
@@ -120,8 +124,22 @@ public class OrderController {
         }
     }
 
-    public void viewInvoice(Context ctx, ConnectionPool connectionPool) {
+    public static void viewInvoice(Context ctx, ConnectionPool connectionPool) {
 
+        int orderId = Integer.parseInt(Objects.requireNonNull(ctx.queryParam("orderId")));
+
+        try {
+            Order order = OrderMapper.getOrderById(orderId, connectionPool);
+            Invoice invoice = OrderMapper.getOrderInvoice(orderId, connectionPool);
+            ctx.attribute("order", order);
+            ctx.attribute("invoice", invoice);
+            Account customer = AccountMapper.getAccountById(order.getAccountId(), connectionPool);
+            ctx.attribute("customer", customer);
+            ctx.render("invoice.html");
+        } catch (DatabaseException e) {
+            ctx.attribute("error", e.getMessage());
+            ctx.render("invoice.html");
+        }
     }
 
     public void cancelOrder(Context ctx, ConnectionPool connectionPool) {
